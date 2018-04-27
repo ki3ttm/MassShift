46b0c3cb548d966f794c02b17ff214213dcc9945
TatsumiKei <ki3ttm@gmail.com>
2018-04-27 20:54:50 +0900

    プロジェクト構成

---
+++ Assets/Scripts/Tatsumi/SimulateBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulateBase : MonoBehaviour {
+	// 種の特徴
+	[SerializeField] float growthRate = 1.0f;       // 成長度
+	[SerializeField] float lifeTimeRate = 10.0f;    // 長命度
+	[SerializeField] float mutationRate = 0.1f;     // 変異度
+
+	[SerializeField] WorldState worldStt = null;
+	public WorldState WorldStt {
+		get {
+			if (worldStt == null) {
+				worldStt = FindObjectOfType<WorldState>();
+			}
+			return worldStt;
+		}
+	}
+
+	[SerializeField] float lifeTime = 0.0f;
+	float LifeTime {
+		get {
+			return lifeTime;
+		}
+		set {
+			lifeTime = value;
+		}
+	}
+
+	// Use this for initialization
+//	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		// 経年劣化
+		LifeTime -= (1.0f / lifeTimeRate) * WorldStt.TimeScl * Time.deltaTime;
+	}
+
+	void Birth(GameObject[] parent = null) {
+		LifeTime = 1.0f;
+	}
+
+	bool isLive() {
+		return (lifeTime > 0.0f);
+	}
+}
---
+++ Assets/Scripts/Tatsumi/WorldState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldState : MonoBehaviour {
+	[SerializeField] float timeScl = 1.0f;
+	public float TimeScl { get { return timeScl; } }
+
+	// Use this for initialization
+//	void Start () {}
+	
+	// Update is called once per frame
+//	void Update () {}
+}
